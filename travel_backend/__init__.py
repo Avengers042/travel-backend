f2df30ae93a5eec625d4ca0f2c87cb3a1cede3dd
victor4pinheiro <victor.pinheiro.soft@proton.me>
2024-08-22 23:22:36 -0300

    :recycle: assign origins allowed to param

--- travel_backend/__init__.py
+++ travel_backend/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from travel_backend.middlewares.cors_settings import CORSSettings
 from travel_backend.routers import api
@@ -9,7 +10,7 @@ cors = CORSSettings()
 
 app.add_middleware(
     cors.middleware,
-    cors.origins,
+    allow_origins=cors.origins,
     allow_credentials=cors.allow_credentials,
     allow_methods=cors.allow_methods,
     allow_headers=cors.allow_headers,
