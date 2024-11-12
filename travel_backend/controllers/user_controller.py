9f4e7bd17ee82d4d08f1f4fc0e676efa21ffe2c8
dev-nuh <developer@nuh.digital>
2024-11-12 20:00:25 -0300

    WIP

--- travel_backend/controllers/user_controller.py
+++ travel_backend/controllers/user_controller.py
@@ -8,25 +8,32 @@ from database.connection import get_session
 from models.preferences import Preferences
 from repositories import user_repository
 from schemas.user.user_schema import UserSchema
+from handlers.clips_handler import recommend_data
 
 
-router = APIRouter(prefix="/user", tags=["user"])
+router = APIRouter(prefix="/preferences", tags=["preferences"])
 
 
 @router.post(
-    "/preferences",
+    "/",
     status_code=200,
     responses={200: {"description": "Problemas nos servidores"}},
 )
 def user_preferences(preferences: Preferences):
     return preferences
 
-@router.get(
-    "/preferences/recommendation",
+@router.post(
+    "/recommendation",
     status_code=200,
     responses={200: {"description": "Problemas nos servidores"}},
 )
-def user_preferences_recommendation():
+def user_preferences_recommendation(body: Any):
+    codeAsProperties = ['natureza', 'cultura', 'cultura e historia', 'aventura', 'vida noturna', 'hotel', 'airbnb', 'hostel', 'acampamento', 'equatorial', 'tropical', 'subtropical', 'massa', 'hamburger', 'sushi', 'churrasco', 'sobremesas']
+
+    recomendantion = recommend_data(codeAsProperties[body['pontoTuristico']['codigo'] - 1], codeAsProperties[body['hospedagem']['codigo'] - 1], codeAsProperties[body['clima']['codigo'] - 1], codeAsProperties[body['alimentacao']['codigo'] - 1])
+
+    print(recomendantion)
+
     example = {
             "place":"Rio de Janeiro",
             "description":"city",
